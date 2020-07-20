2f25048a34b4faf340535e08915cf911077f2e0c
Jake Robertson <jake@faltro.com>
2020-07-20 18:15:39 -0400

    create electron app

--- src/main/java/com/faltro/houdoku/exception/ContentUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package com.faltro.houdoku.exception;
-
-public class ContentUnavailableException extends Exception {
-    public ContentUnavailableException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/faltro/houdoku/exception/LicensedContentException.java
+++
@@ -1,7 +0,0 @@
-package com.faltro.houdoku.exception;
-
-public class LicensedContentException extends ContentUnavailableException {
-    public LicensedContentException(String message) {
-        super(message);
-    }
-}
