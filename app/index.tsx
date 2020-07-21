192ca2e0f19a1434f40a2b67f54212ac79a1ee69
Jake Robertson <jake@faltro.com>
2020-07-21 03:14:26 -0400

    add react boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
+import Root from './containers/Root';
+import { history, configuredStore } from './store';
+import './app.global.css';
+
+const store = configuredStore();
+
+const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+
+document.addEventListener('DOMContentLoaded', () =>
+  render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+);
