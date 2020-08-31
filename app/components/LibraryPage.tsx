b9778f3b910428e733878b46db867b5f748ac849
Jake Robertson <jake@faltro.com>
2020-08-30 22:43:58 -0400

    add drawer to library page

--- app/components/LibraryPage.tsx
+++ app/components/LibraryPage.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
 // import { Link } from 'react-router-dom';
 // import routes from '../constants/routes.json';
 import { RootState } from '../store';
@@ -32,28 +42,94 @@ const mapDispatch = (dispatch: any) => ({
 const connector = connect(mapState, mapDispatch);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-type LibraryProps = PropsFromRedux & {
+type Props = PropsFromRedux & {
   something: string;
 };
 
-const LibraryPage = (props: LibraryProps) => (
-  <div>
-    <p>this is the top of the page.</p>
-    <Button onClick={props.updateSeriesList}>update the series list</Button>
-    <Button onClick={() => props.changeNumColumns(8)}>
-      update num columns
-    </Button>
-    <Button onClick={() => props.saveLibrary()}>save library</Button>
-    <Button onClick={() => props.readLibrary()}>read library</Button>
-    <Button onClick={() => props.deleteLibrary()}>delete library</Button>
-    {props.library != null && (
-      <LibraryGrid
-        columns={props.columns}
-        seriesList={props.library.seriesList}
-      />
-    )}
-    {/* <Link to={routes.SERIES}>to Series</Link> */}
-  </div>
-);
+const drawerWidth = 150;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
+const LibraryPage: React.FC<Props> = (props: Props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Library
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <p>this is the top of the page.</p>
+        <Button onClick={props.updateSeriesList}>update the series list</Button>
+        <Button onClick={() => props.changeNumColumns(8)}>
+          update num columns
+        </Button>
+        <Button onClick={() => props.saveLibrary()}>save library</Button>
+        <Button onClick={() => props.readLibrary()}>read library</Button>
+        <Button onClick={() => props.deleteLibrary()}>delete library</Button>
+        {props.library != null && (
+          <LibraryGrid
+            columns={props.columns}
+            seriesList={props.library.seriesList}
+          />
+        )}
+        {/* <Link to={routes.SERIES}>to Series</Link> */}
+      </main>
+    </div>
+  );
+};
 
 export default connector(LibraryPage);
