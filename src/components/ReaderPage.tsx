5feb866c3440f74c2dbc9cf65a921e8ffe58dc4d
Jake Robertson <jake@faltro.com>
2021-03-14 17:58:59 -0400

    move readerviewer to separate component

--- src/components/ReaderPage.tsx
+++ src/components/ReaderPage.tsx
@@ -43,8 +43,7 @@ import {
 import { toggleChapterRead } from '../features/library/utils';
 import { useForceUpdate } from '../util/reactutil';
 import ReaderSidebar from './ReaderSidebar';
-
-const { Content } = Layout;
+import ReaderViewer from './ReaderViewer';
 
 const KEYBOARD_SHORTCUTS = {
   previousPage: 'left',
@@ -183,49 +182,6 @@ const ReaderPage: React.FC<Props> = (props: Props) => {
     }
   };
 
-  const getPageMargin = () => {
-    return `${props.pageNumber * -100 + 100}%`;
-  };
-
-  const renderPageImage = (pageNumber: number) => {
-    if (props.series === undefined) return <></>;
-    if (props.pageUrls.length === 0) return <></>;
-
-    return pageNumber <= props.lastPageNumber && pageNumber > 0 ? (
-      <img
-        className={styles.pageImage}
-        src={props.pageDataList[pageNumber - 1]}
-        alt={`page${pageNumber}`}
-        loading="lazy"
-      />
-    ) : (
-      <img className={styles.pageImage} src="data:," alt="" />
-    );
-  };
-
-  const renderTwoPageLayout = (pageNumber: number) => {
-    const firstPageNumber =
-      props.pageView === PageView.Double_OddStart ? pageNumber - 1 : pageNumber;
-    return (
-      <>
-        <span className={styles.imageColumn}>
-          {renderPageImage(
-            props.layoutDirection === LayoutDirection.LeftToRight
-              ? firstPageNumber
-              : firstPageNumber + 1
-          )}
-        </span>
-        <span className={styles.imageColumn}>
-          {renderPageImage(
-            props.layoutDirection === LayoutDirection.LeftToRight
-              ? firstPageNumber + 1
-              : firstPageNumber
-          )}
-        </span>
-      </>
-    );
-  };
-
   const renderPreloadContainer = (pageNumber: number) => {
     if (props.series === undefined) return <></>;
     if (props.pageUrls.length === 0) return <></>;
@@ -243,30 +199,6 @@ const ReaderPage: React.FC<Props> = (props: Props) => {
     }
 
     return <div className={styles.preloadContainer}>{images}</div>;
-  };
-
-  const renderViewer = () => {
-    const imageWrappers = [];
-
-    for (let i = 1; i <= props.lastPageNumber; i += 1) {
-      imageWrappers.push(
-        <Content
-          key={i}
-          className={`${styles.imageWrapper}
-            ${props.pageFit === PageFit.Auto ? styles.fitAuto : ''}
-            ${props.pageFit === PageFit.Width ? styles.fitWidth : ''}
-            ${props.pageFit === PageFit.Height ? styles.fitHeight : ''}
-          `}
-          style={{ marginLeft: i === 1 ? getPageMargin() : 0 }}
-        >
-          {props.pageView === PageView.Single
-            ? renderPageImage(i)
-            : renderTwoPageLayout(i)}
-        </Content>
-      );
-    }
-
-    return <div className={styles.viewerContainer}>{imageWrappers}</div>;
   };
 
   const changePage = (left: boolean, toBound = false) => {
@@ -398,7 +330,7 @@ const ReaderPage: React.FC<Props> = (props: Props) => {
       />
       <Layout className={`site-layout ${styles.contentLayout}`}>
         {renderPreloadContainer(props.pageNumber)}
-        {renderViewer()}
+        <ReaderViewer />
       </Layout>
     </Layout>
   );
