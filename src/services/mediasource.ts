72334e1f86f1d3ad37b5ce3710854466c93be6ee
Jake Robertson <jake@faltro.com>
2021-03-15 18:21:24 -0400

    add doc comments for mediasource functions

--- src/services/mediasource.ts
+++ src/services/mediasource.ts
@@ -1,6 +1,11 @@
 import { Series } from '../models/types';
 import anilist from './mediasource/anilist';
 
+/**
+ * Get the banner image URL for a series.
+ * @param series
+ * @returns promise for the banner URL, if available, else null
+ */
 // eslint-disable-next-line import/prefer-default-export
 export function getBannerImageUrl(series: Series) {
   return anilist
--- src/services/mediasource/interface.ts
+++ src/services/mediasource/interface.ts
@@ -1,9 +1,22 @@
 import { Series } from '../../models/types';
 
+/**
+ * Request a banner image URL from the media source.
+ * The banner is for display at the top of the series details page.
+ *
+ * @param series the series to get the banner image for
+ * @returns Promise<Response> to be handled by ParseBannerImageUrlFunc
+ */
 export interface FetchBannerImageUrlFunc {
   (series: Series): Promise<Response>;
 }
 
+/**
+ * Parse the response from FetchBannerImageUrlFunc
+ *
+ * @param json the json content of the response
+ * @returns the banner image URL, if found, else null
+ */
 export interface ParseBannerImageUrlFunc {
   (json: any): string | null;
 }
