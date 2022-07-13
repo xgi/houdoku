63225df4e87cfa11cc6221d2815857b84202973e
Jake Robertson <jake@faltro.com>
2022-07-13 16:19:25 -0700

    revert react & electron version updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
 import App from './App';
 import './App.global.css';
 import { ErrorBoundary } from './components/general/ErrorBoundary';
 import { createWindowControlListeners } from './util/titlebar';
 
 createWindowControlListeners();
-
-const container = document.getElementById('root');
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!);
-root.render(
+render(
   <ErrorBoundary>
     <App />
-  </ErrorBoundary>
+  </ErrorBoundary>,
+  document.getElementById('root')
 );
