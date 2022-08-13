b1eb7f31dea9f89288942a337b2a81d65ae6ba33
Jake Robertson <jake@faltro.com>
2022-08-13 14:57:54 -0700

    add merging to backup restore logic

--- src/util/backup.ts
+++ src/util/backup.ts
@@ -1,15 +1,16 @@
+import { Chapter, Series } from 'houdoku-extension-lib';
 import storeKeys from '../constants/storeKeys.json';
-import persistantStore from './persistantStore';
+import { updateSeries } from '../features/library/utils';
+import library from '../services/library';
 
 export const createBackup = async () => {
-  const seriesListStr = persistantStore.read(storeKeys.LIBRARY.SERIES_LIST);
-  const blob = new Blob([JSON.stringify(seriesListStr)], {
+  const blob = new Blob([JSON.stringify(localStorage)], {
     type: 'application/json',
   });
 
   const link = document.createElement('a');
   link.href = URL.createObjectURL(blob);
-  link.download = `houdoku_series_backup_${new Date().toJSON().slice(0, 10)}.json`;
+  link.download = `houdoku_backup_${new Date().toJSON().slice(0, 10)}.json`;
 
   document.body.appendChild(link);
   link.click();
@@ -17,5 +18,33 @@ export const createBackup = async () => {
 };
 
 export const restoreBackup = (backupFileContent: string) => {
-  persistantStore.write(storeKeys.LIBRARY.SERIES_LIST, JSON.parse(backupFileContent));
+  const data: { [key: string]: string } = JSON.parse(backupFileContent);
+
+  // add series' from the backup into the library
+  if (storeKeys.LIBRARY.SERIES_LIST in data) {
+    const oldSeriesList: Series[] = JSON.parse(data[storeKeys.LIBRARY.SERIES_LIST]);
+    Object.values(oldSeriesList).forEach((series: Series) => updateSeries(series));
+  }
+
+  // add chapters from backup while maintaining progress from current & backup
+  Object.entries(data).forEach(([key, value]) => {
+    if (key.startsWith(storeKeys.LIBRARY.CHAPTER_LIST_PREFIX)) {
+      const seriesId = key.split(storeKeys.LIBRARY.CHAPTER_LIST_PREFIX)[1];
+      const series = library.fetchSeries(seriesId);
+      if (!series) return;
+
+      const existingChapters = library.fetchChapters(seriesId);
+      const oldChapters: Chapter[] = JSON.parse(value);
+
+      const chaptersToSave: Chapter[] = [];
+      oldChapters.forEach((oldChapter) => {
+        const existingChapter = existingChapters.find((c) => c.id === oldChapter.id);
+        chaptersToSave.push({
+          ...oldChapter,
+          read: (existingChapter && existingChapter.read) || oldChapter.read,
+        });
+      });
+      library.upsertChapters(chaptersToSave, series);
+    }
+  });
 };
