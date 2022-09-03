7c72d6d40b7087724a54a9c53e3e173a56f05132
Jake Robertson <jake@faltro.com>
2022-09-03 04:28:01 -0700

    mantine: replace all settings page components

--- src/components/settings/TrackerSettings.tsx
+++ src/components/settings/TrackerSettings.tsx
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, Col, Input, Row, Spin, Collapse, Typography, Switch } from 'antd';
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, shell } from 'electron';
 import log from 'electron-log';
 import { useRecoilState } from 'recoil';
-import styles from './TrackerSettings.css';
+import {
+  Accordion,
+  Text,
+  Button,
+  Group,
+  Code,
+  Stepper,
+  Alert,
+  Grid,
+  Switch,
+  Input,
+  Loader,
+} from '@mantine/core';
+import { IconExternalLink, IconInfoCircle } from '@tabler/icons';
 import ipcChannels from '../../constants/ipcChannels.json';
 import storeKeys from '../../constants/storeKeys.json';
 import persistantStore from '../../util/persistantStore';
@@ -11,9 +23,6 @@ import { AniListTrackerMetadata } from '../../services/trackers/anilist';
 import { TrackerSetting } from '../../models/types';
 import { MALTrackerMetadata } from '../../services/trackers/myanimelist';
 import { trackerAutoUpdateState } from '../../state/settingStates';
-
-const { Text, Paragraph } = Typography;
-const { Panel } = Collapse;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {};
@@ -87,112 +96,109 @@ const TrackerSettings: React.FC<Props> = (props: Props) => {
 
   if (loading) {
     return (
-      <div className={styles.loaderContainer}>
-        <Spin />
-        <Paragraph>Reloading tracker details</Paragraph>
-      </div>
+      <Group position="center">
+        <Loader />
+        <Text>Reloading tracker details...</Text>
+      </Group>
     );
   }
 
   return (
     <>
       <Alert
-        className={styles.infoAlert}
-        type="info"
-        message='Houdoku allows you to sync your account on list-tracking websites and
-        automatically upload your progress as you read. After authenticating
-        below, click the "Trackers" button on a series page to link it
-        with an entry on your list.'
-      />
-      <Row className={styles.row}>
-        <Col span={10}>Update Progress Automatically</Col>
-        <Col span={14}>
+        icon={<IconInfoCircle size={16} />}
+        title="Syncing progress with trackers"
+        color="indigo"
+      >
+        Houdoku allows you to sync your account on list-tracking websites and automatically upload
+        your progress as you read. After authenticating, click the &quot;Trackers&quot; button on a
+        series page to link it with an entry on your list.
+      </Alert>
+
+      <Grid pt="sm">
+        <Grid.Col span={5}>Update Progress Automatically</Grid.Col>
+        <Grid.Col span={7}>
           <Switch
+            size="md"
             checked={trackerAutoUpdate}
-            onChange={(checked: boolean) =>
-              updateTrackerSetting(TrackerSetting.TrackerAutoUpdate, checked)
+            onChange={(e) =>
+              updateTrackerSetting(TrackerSetting.TrackerAutoUpdate, e.target.checked)
             }
           />
-        </Col>
-      </Row>
-      <Collapse>
+        </Grid.Col>
+      </Grid>
+
+      <Accordion chevronPosition="left" mx="auto" pt="sm">
         {[AniListTrackerMetadata, MALTrackerMetadata].map((trackerMetadata) => (
-          <Panel
-            key={trackerMetadata.id}
-            // header={trackerMetadata.name}
-            header={
-              <div
-                style={{
-                  width: 'calc(100% - 24px)',
-                  display: 'inline-flex',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <span>{trackerMetadata.name}</span>
+          <Accordion.Item value={trackerMetadata.id}>
+            <Accordion.Control>
+              <Group position="apart">
+                <Text>{trackerMetadata.name}</Text>
                 {usernames[trackerMetadata.id] ? (
-                  <div>
-                    <span>
-                      Logged in as <Text code>{usernames[trackerMetadata.id]}</Text>
-                    </span>
+                  <Group position="right">
+                    <Text>
+                      Logged in as <Code>{usernames[trackerMetadata.id]}</Code>
+                    </Text>
                     <Button
-                      className={styles.unlinkButton}
-                      type="primary"
-                      danger
-                      size="small"
-                      onClick={() => saveAccessToken(trackerMetadata.id, '')}
+                      compact
+                      color="red"
+                      radius={0}
+                      onClick={(e: any) => {
+                        e.stopPropagation();
+                        saveAccessToken(trackerMetadata.id, '');
+                      }}
                     >
                       Unlink
                     </Button>
-                  </div>
+                  </Group>
                 ) : (
-                  <span>Not logged in.</span>
+                  <Text>Not logged in.</Text>
                 )}
-              </div>
-            }
-          >
-            <Row className={styles.step}>
-              <Col span={6}>1) Open the authentication page in your browser</Col>
-              <Col span={2} />
-              <Col span={16}>
-                <Paragraph>
-                  <a href={authUrls[trackerMetadata.id]} target="_blank" rel="noreferrer">
-                    {authUrls[trackerMetadata.id]}
-                  </a>
-                </Paragraph>
-              </Col>
-            </Row>
-            <Row className={styles.step}>
-              <Col span={6}>2) Copy the code you receive</Col>
-              <Col span={2} />
-              <Col span={16}>
-                <Input
-                  value={accessCodes[trackerMetadata.id]}
-                  onChange={(e: any) =>
-                    setAccessCodes({
-                      ...accessCodes,
-                      [trackerMetadata.id]: e.target.value,
-                    })
+              </Group>
+            </Accordion.Control>
+            <Accordion.Panel>
+              <Stepper active={-1} orientation="vertical">
+                <Stepper.Step
+                  label={
+                    <Button
+                      variant="default"
+                      leftIcon={<IconExternalLink />}
+                      onClick={() => shell.openExternal(authUrls[trackerMetadata.id])}
+                    >
+                      Authenticate on {trackerMetadata.name}
+                    </Button>
                   }
                 />
-              </Col>
-            </Row>
-            <Row className={styles.step}>
-              <Col span={6}>3) Save details</Col>
-              <Col span={2} />
-              <Col span={16}>
-                <Button
-                  onClick={() =>
-                    submitAccessCode(trackerMetadata.id, accessCodes[trackerMetadata.id])
+                <Stepper.Step
+                  label={
+                    <Input
+                      placeholder="Paste access code..."
+                      value={accessCodes[trackerMetadata.id]}
+                      onChange={(e: any) =>
+                        setAccessCodes({
+                          ...accessCodes,
+                          [trackerMetadata.id]: e.target.value,
+                        })
+                      }
+                    />
                   }
-                >
-                  Submit
-                </Button>
-              </Col>
-            </Row>
-          </Panel>
+                />
+                <Stepper.Step
+                  label={
+                    <Button
+                      onClick={() =>
+                        submitAccessCode(trackerMetadata.id, accessCodes[trackerMetadata.id])
+                      }
+                    >
+                      Submit
+                    </Button>
+                  }
+                />
+              </Stepper>
+            </Accordion.Panel>
+          </Accordion.Item>
         ))}
-      </Collapse>
+      </Accordion>
     </>
   );
 };
