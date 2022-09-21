5c2b3ee623ef9a673d2a13ec4dbad8d2f183dde5
Jake Robertson <jake@faltro.com>
2022-09-21 13:24:00 -0700

    update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import './App.global.css';
 import { createWindowControlListeners } from './util/titlebar';
 
 createWindowControlListeners();
-render(
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
   <RecoilRoot>
     <App />
-  </RecoilRoot>,
-  document.getElementById('root')
+  </RecoilRoot>
 );
