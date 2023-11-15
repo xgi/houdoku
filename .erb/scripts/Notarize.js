99f61b9c2cc723aad2955c7988d0a5eab688164b
Jake Robertson <jake@faltro.com>
2023-11-14 20:48:53 -0800

    fix electron/notarize import

--- .erb/scripts/Notarize.js
+++ .erb/scripts/Notarize.js
@@ -1,4 +1,4 @@
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 const { build } = require('../../package.json');
 
 exports.default = async function notarizeMacos(context) {
@@ -13,7 +13,9 @@ exports.default = async function notarizeMacos(context) {
   }
 
   if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
-    console.warn('Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set');
+    console.warn(
+      'Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set'
+    );
     return;
   }
 
