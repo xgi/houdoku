5eea3c6aa869c83570fdf084d4eb19c3ef9e2f99
Jake Robertson <jake@faltro.com>
2024-10-05 18:47:17 -0700

    fix list layout not honoring remove modal setting

--- src/renderer/components/library/LibraryList.tsx
+++ src/renderer/components/library/LibraryList.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Series } from '@tiyo/common';
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { ActionIcon, Badge, Button, Group, Table, Text } from '@mantine/core';
 import { IconTrash } from '@tabler/icons';
-import { goToSeries } from '@/renderer/features/library/utils';
+import { goToSeries, removeSeries } from '@/renderer/features/library/utils';
 import { seriesListState } from '@/renderer/state/libraryStates';
+import { confirmRemoveSeriesState } from '@/renderer/state/settingStates';
 
 type Props = {
   getFilteredList: () => Series[];
@@ -15,9 +16,20 @@ type Props = {
 const LibraryList: React.FC<Props> = (props: Props) => {
   const navigate = useNavigate();
   const setSeriesList = useSetRecoilState(seriesListState);
+  const confirmRemoveSeries = useRecoilValue(confirmRemoveSeriesState);
 
   const viewFunc = (series: Series) => {
     goToSeries(series, setSeriesList, navigate);
+  };
+
+  const removeFunc = (series: Series) => {
+    if (series) {
+      if (confirmRemoveSeries) {
+        props.showRemoveModal(series);
+      } else {
+        removeSeries(series, setSeriesList);
+      }
+    }
   };
 
   const renderRows = () => {
@@ -37,12 +49,7 @@ const LibraryList: React.FC<Props> = (props: Props) => {
           <td>
             <Group gap="xs" justify="flex-end" wrap="nowrap">
               <Button onClick={() => viewFunc(series)}>View</Button>
-              <ActionIcon
-                variant="filled"
-                color="red"
-                size="lg"
-                onClick={() => props.showRemoveModal(series)}
-              >
+              <ActionIcon variant="filled" color="red" size="lg" onClick={() => removeFunc(series)}>
                 <IconTrash size={20} />
               </ActionIcon>
             </Group>
