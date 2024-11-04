7cb28330e95670eda5f529bf44bfa5e6c8ebe9d0
Jake Robertson <jake@faltro.com>
2024-11-03 22:12:42 -0800

    fix local alias path resolution for windows

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,32 +1,32 @@
-import path from 'path';
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import path from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
     resolve: {
       alias: {
-        '@/': path.join(__dirname, 'src/'),
+        "@": path.join(__dirname, "src"),
       },
     },
   },
   preload: {
     resolve: {
       alias: {
-        '@/': path.join(__dirname, 'src/'),
+        "@": path.join(__dirname, "src"),
       },
     },
   },
   renderer: {
     plugins: [
       nodePolyfills({
-        include: ['path', 'fs', 'constants', 'stream', 'util', 'zlib'],
+        include: ["path", "fs", "constants", "stream", "util", "zlib"],
       }),
     ],
     resolve: {
       alias: {
-        '@/': path.join(__dirname, 'src/'),
+        "@": path.join(__dirname, "src"),
       },
     },
   },
